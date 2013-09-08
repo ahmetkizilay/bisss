960d1ec0d3ae99f87f47644099439fa9b58c64b6
Ahmet Kizilay <ahmet.kizilay@gmail.com>
2013-09-08 15:09:56 +0300

    basic functionality implemented
    the first argument is considered to be the root folder. Basic necessary files are created, and truncated if they already existed. Later on default texts could be added to the files, and the list of files to be creted could be read from a config file...

--- app.js
+++ app.js
@@ -1,5 +1,60 @@
-(function () {
-    'use strict';
+String.prototype.col = function(col) {
+    switch(col) {
+        case 'black':
+            return '\033[30m' + this + '\033[0m';
+        case 'red':
+            return '\033[31m' + this + '\033[0m';
+        case 'green':
+            return '\033[32m' + this + '\033[0m';
+        case 'yellow':
+            return '\033[33m' + this + '\033[0m';
+        case 'blue':
+            return '\033[34m' + this + '\033[0m';
+        case 'magenta':
+            return '\033[35m' + this + '\033[0m';
+        case 'cyan':
+            return '\033[36m' + this + '\033[0m';
+        case 'white':
+            return '\033[37m' + this + '\033[0m';
+        default:
+            return this;
+    }
+};
 
-    console.log('you just ran me');
-}).call();
+module.exports = function (argv) {
+    'use strict';
+    var path = require('path'),
+        fs = require('fs'),
+        rootPath;
+
+    if(argv.length < 3) {
+        console.log('error: expected another argument for root path');
+        return;
+    }
+    
+    rootPath = path.join(__dirname, argv[2]);
+
+    if(fs.existsSync(rootPath)) {
+        console.log('exists: '.col('red') + rootPath);
+    }
+    else {
+        fs.mkdirSync(rootPath);
+        console.log('created: '.col('blue') + rootPath);
+    }
+
+    // creating default files... files to be created could be read from a config file
+    fs.openSync(path.join(rootPath, 'README.md'), 'w');
+    console.log('created: '.col('blue') + path.join(rootPath, 'README.md'));
+
+    fs.openSync(path.join(rootPath, 'package.json'), 'w');
+    console.log('created: '.col('blue') + path.join(rootPath, 'package.json'));
+
+    fs.openSync(path.join(rootPath, 'app.js'), 'w');
+    console.log('created: '.col('blue') + path.join(rootPath, 'app.js'));
+
+    fs.openSync(path.join(rootPath, '.gitignore'), 'w');
+    console.log('created: '.col('blue') + path.join(rootPath, '.gitignore'));
+
+    fs.openSync(path.join(rootPath, 'LICENSE'), 'w');
+    console.log('created: '.col('blue') + path.join(rootPath, 'LICENSE'));
+};
