fbfe8c9d227d40c40607e4f352b30fcb2d4f162e
Ahmet Kizilay <ahmet.kizilay@gmail.com>
2013-09-08 17:51:51 +0300

    refactored method, added git init

--- app.js
+++ app.js
@@ -21,46 +21,102 @@ String.prototype.col = function(col) {
     }
 };
 
-module.exports = function (argv) {
+module.exports = (function () {
     'use strict';
     var path = require('path'),
-        fs = require('fs'),
-        rootPath, projectName;
+        fs = require('fs');
 
-    if(argv.length < 3) {
-        console.log('error: expected another argument for root path');
-        return;
-    }
+    var _initGit = function (rootPath) {
+        var sys = require('sys'),
+            exec = require('child_process').exec;
+
+        exec("git init " + rootPath, function (err, stdout, stderr) {
+            if(err) {
+                throw err;
+            }
+            console.log('initializing git...'.col('blue'));
+            sys.puts(stdout);
+        });
+    };
+
+    var _initPackageJSON = function (rootPath, projectName) {
+        var pkg = '';
+        pkg += '{\n';
+        pkg += '    "name": "' + projectName + '",\n';
+        pkg += '    "description": "",\n';
+        pkg += '    "version: "0.0.0",\n';
+        pkg += '    "private": true,\n';
+        pkg += '    "author": "",\n';
+        pkg += '    "license": "",\n';
+        pkg += '    "dependencies": {},\n';
+        pkg += '    "repository": {\n';
+        pkg += '        "type": "",\n';
+        pkg += '        "url": ""\n';
+        pkg += '    }\n';
+        pkg += "}";
+
+        fs.writeFile(path.join(rootPath, 'package.json'), pkg, function (err) {
+            if(err) {
+                throw err;
+            }
+
+            console.log('created: '.col('blue') + path.join(rootPath, 'package.json'));
+        });
+    };
+
+    var _initReadme = function (rootPath, projectName) {
+        var str = '# ' + projectName;
+
+        fs.writeFile(path.join(rootPath, 'README.md'), str, function (err) {
+            if(err) {
+                throw err;
+            }
+
+            console.log('created: '.col('blue') + path.join(rootPath, 'README.md'));
+        });
+    };
+
+    var _mainFunc = function (argv) {
     
-    projectName = argv[2];
-    rootPath = path.join(__dirname, projectName);
+        var rootPath, projectName, rootDir;
 
-    if(fs.existsSync(rootPath)) {
-        console.log('exists: '.col('red') + rootPath);
-    }
-    else {
-        fs.mkdirSync(rootPath);
-        console.log('created: '.col('blue') + rootPath);
-    }
+        if(argv.length < 3) {
+            console.log('error: expected another argument for root path');
+            return;
+        }
+        
+        projectName = argv[2];
+        rootDir = argv[3] || '.';
+        rootPath = path.join(__dirname, rootDir, projectName);
 
-    // creating default files... files to be created could be read from a config file
-    fs.writeFile(path.join(rootPath, 'README.md'), '# ' + projectName, function (err) {
-        if(err) {
-            throw err;
+        if(fs.existsSync(rootPath)) {
+            console.log('exists: '.col('red') + rootPath);
+        }
+        else {
+            fs.mkdirSync(rootPath);
+            console.log('created: '.col('blue') + rootPath);
         }
 
-        console.log('created: '.col('blue') + path.join(rootPath, 'README.md'));
-    });
+        // creating default files... files to be created could be read from a config file
+        _initReadme(rootPath, projectName);
+        _initPackageJSON(rootPath, projectName);
 
-    fs.openSync(path.join(rootPath, 'package.json'), 'w');
-    console.log('created: '.col('blue') + path.join(rootPath, 'package.json'));
+        fs.openSync(path.join(rootPath, 'app.js'), 'w');
+        console.log('created: '.col('blue') + path.join(rootPath, 'app.js'));
 
-    fs.openSync(path.join(rootPath, 'app.js'), 'w');
-    console.log('created: '.col('blue') + path.join(rootPath, 'app.js'));
+        fs.writeFile(path.join(rootPath, '.gitignore'), 'node_modules/', function (err) {
+            if(err) {
+                throw err;
+            }
 
-    fs.openSync(path.join(rootPath, '.gitignore'), 'w');
-    console.log('created: '.col('blue') + path.join(rootPath, '.gitignore'));
+            console.log('created: '.col('blue') + path.join(rootPath, '.gitignore'));
+        });
 
-    fs.openSync(path.join(rootPath, 'LICENSE'), 'w');
-    console.log('created: '.col('blue') + path.join(rootPath, 'LICENSE'));
-};
+        fs.openSync(path.join(rootPath, 'LICENSE'), 'w');
+        console.log('created: '.col('blue') + path.join(rootPath, 'LICENSE'));
+
+        _initGit(rootPath);
+    };
+
+    return _mainFunc;
+}());
